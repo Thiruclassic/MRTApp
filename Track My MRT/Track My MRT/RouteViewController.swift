a3f9cc0604770a1864834e098ffaf81a37d6c16d
Thirumal Dhanasekaran <thirumaldhanasekaran@thirumals-imac.local>
2017-05-03 06:02:34 -0700

    route view controller changes

--- Track My MRT/Track My MRT/RouteViewController.swift
+++ Track My MRT/Track My MRT/RouteViewController.swift
@@ -11,8 +11,7 @@ import UIKit
 class RouteViewController : UIViewController{
     
     
-    var fromStationText:String!
-    var toStationText:String!
+    var stationData:StationData!
     
     @IBOutlet weak var fromStationLabel: UILabel!
     
@@ -35,13 +34,105 @@ class RouteViewController : UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        fromStationLabel.text=fromStationText
-        fromStationScrollLabel.text=fromStationText
-        toStationLabel.text=toStationText
+        fromStationLabel.text=stationData.fromStation
+        fromStationScrollLabel.text=stationData.fromStation
+        toStationLabel.text=stationData.toStation
+        fareLabel.text=stationData.fare!
         
-       // readStationArrivalTime(stnCode: "asd")
+        arrivalTimeLabel.text = stationData.arrivalTime
+        nextArrivalTimeLabel.text = stationData.nxtTrainArrivalTime
+        
+        readStationArrivalTime(stnCode: stationData.stationCode,stnDirectionId: stationData.stationDirectionId)
     
+    }
+    
+    func readStationArrivalTime(stnCode:String,stnDirectionId:Int)
+    {
+        let endpoint: String = MRT_ARRTIVAL_URL
+        guard let mrtURL = URL(string: endpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        var arrivalTimes:[String]!
+        
+        var mrtUrlRequest = URLRequest(url: mrtURL)
+        
+        
+        let postString : String?
+        postString=POST_KEY + stnCode
+        
+        mrtUrlRequest.httpBody=postString!.data(using: String.Encoding.utf8)
+        
+        mrtUrlRequest.httpMethod = "POST"
+        
+        
+        
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: mrtUrlRequest) {
+            (data, response, error) in
+            guard error == nil else {
+                print("error calling POST on API")
+                print(error)
+                return
+            }
+            
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            
+            
+            let httpStatus = response as? HTTPURLResponse
+            print("status: \(httpStatus?.statusCode)")
+            // parse the result as JSON, since that's what the API provides
+            do {
+                guard let receivedMRTData = try JSONSerialization.jsonObject(with: responseData,
+                                                                             options: []) as? [String: Any] else {
+                                                                                print("Could not get JSON from responseData as dictionary")
+                                                                                return
+                }
+                //print("Train Arrival Data: " + receivedMRTData.description)
+                
+                guard let stnData = receivedMRTData[stnCode] as? AnyObject,let platformData=stnData[PLATFORM+String(stnDirectionId)] as? AnyObject, let nxtTrain=platformData[NXT_TRAIN] as? String,let subTrain=platformData[SUB_TRAIN] as? String,let time=receivedMRTData[TIME] as? String
+                    else {
+                        print("Could not get MRTData from JSON")
+                        return
+                }
+                
+                let subTrainText = subTrain.replacingOccurrences(of: REDUNTANT_TEXT, with: "")
+                let nextTrainText = nxtTrain.replacingOccurrences(of: REDUNTANT_TEXT, with: "")
+                
+                
+                
+                
+                
+                arrivalTimes = computeLocalArrivalTime(nextTrain: nextTrainText, subTrain: subTrainText, time: time)
+                
+                print(arrivalTimes[0])
+                print(arrivalTimes[1])
+                
+                DispatchQueue.main.async {
+                    self.arrivalTimeLabel.text=arrivalTimes[0]
+                    self.nextArrivalTimeLabel.text=arrivalTimes[1]
+                }
+                
+                
+                
+                
+                
+            } catch let err {
+                print("error parsing response from POST on mrt api")
+                print(err.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+        
     }
     
     
 }
+
+
