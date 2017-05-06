d599b47f58b01fac9106feab9369d9d2c7702aba
Varun Sam <varunsam1094@gmail.com>
2017-05-06 19:05:00 +0800

    adding station model

---
+++ Track My MRT/Track My MRT/StationModel.swift
@@ -0,0 +1,23 @@
+//
+//  StationModel.swift
+//  Track My MRT
+//
+//  Created by Varun Sam on 06/05/17.
+//  Copyright © 2017 Team-FT03. All rights reserved.
+//
+
+import Foundation
+
+
+class StationModel{
+    
+    var stationPrimaryId:Int!
+    var stationName:String!
+    var stationCode:String!
+    var stationDistance:[String] = [String] ()
+    var stationLineCodes:[String] = [String] ()
+    var stationLaneCodes:[String] = [String] ()
+    var stationNumber:String!
+    var colors:[String] = [String] ()
+    
+}
