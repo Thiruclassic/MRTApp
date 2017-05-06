3221e46abb1ed33a04969c84e06a57c01c12e541
Varun Sam <varunsam1094@gmail.com>
2017-05-06 18:49:09 +0800

    database handler changes

--- Track My MRT/Track My MRT/DbHandler.swift
+++ Track My MRT/Track My MRT/DbHandler.swift
@@ -19,11 +19,11 @@ func createTables()
 {
     if(openDatabase())
     {
-    //dropTables()
+
     createStationTable()
     createDistanceTable()
-    //createStationLaneTable()
-    //createCheckpointTable()
+    createStationLaneTable()
+    createCheckpointTable()
     }
     
    
@@ -94,12 +94,15 @@ func createCheckpointTable()
 {
     if(executeSql(sql: CHECKPOINT_CREATE_QUERY, tableName: CHECKPOINT_TABLE))
     {
-        createStationData()
+        createCheckpointData()
     }
 }
 
 func dropTables()
 {
+    
+    if(openDatabase())
+    {
     var sql="DROP TABLE MRTSTATION"
     
     executeSql(sql: sql, tableName: MRTSTATION_TABLE)
@@ -112,9 +115,10 @@ func dropTables()
     
     executeSql(sql: sql, tableName: STATIONLANE_TABLE)
     
-    sql="DROP TABLE CHECKPOINT"
+    sql="DROP TABLE CHECKPOINTSTATION"
     
     executeSql(sql: sql, tableName: CHECKPOINT_TABLE)
+    }
     
 }
 
@@ -209,15 +213,15 @@ func createCheckpointData()
     var count:Int = 0
     for checkpoint in checkpoints
     {
-        let lineCode=checkpoint[STATION_LINE_CODE] as? NSString
-        let totalStations=checkpoint[STATION_LANE_TOTAL_STATIONS] as? NSString
-        let totalDistance=checkpoint[STATION_LANE_TOTAL_DISTANCE] as? NSString
-        let laneColor=checkpoint[STATION_LANE_COLOR] as? NSString
+        let checkpointName=checkpoint[CHECKPOINT_NAME] as? NSString
+        let stationLineID=checkpoint[CHECKPOINT_STATION_LINE_ID] as? NSString
+        let stationNo=checkpoint[CHECKPOINT_STATION_ID] as? NSString
+    
         
-        sqlite3_bind_text(insertStatement, 1, lineCode!.utf8String, -1, SQLITE_TRANSIENT);
-        sqlite3_bind_text(insertStatement, 2, totalStations!.utf8String , -1, SQLITE_TRANSIENT);
-        sqlite3_bind_text(insertStatement, 3, totalDistance!.utf8String , -1, SQLITE_TRANSIENT);
-        sqlite3_bind_text(insertStatement, 4, laneColor!.utf8String , -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(insertStatement, 1, checkpointName!.utf8String, -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(insertStatement, 2, stationLineID!.utf8String , -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(insertStatement, 3, stationNo!.utf8String , -1, SQLITE_TRANSIENT);
+        
         
         count=count+1
         let returnCode=sqlite3_step(insertStatement)
@@ -235,7 +239,7 @@ func createCheckpointData()
         sqlite3_clear_bindings(insertStatement);
     }
     
-    print("Station Lane Data inserted successfully")
+    print("\(CHECKPOINT_TABLE) Data inserted successfully")
 
     
 }
@@ -274,7 +278,7 @@ func createDistanceFareData()
     sqlite3_clear_bindings(insertStatement);
     }
     
-    print("Checkpoint Data inserted successfully")
+    print("Distance Fare Data inserted successfully")
 
 
 }
@@ -332,6 +336,13 @@ func createStationData()
         let name = station[STATION_NAME] as? NSString
         let code=station[STATION_CODE] as? NSString
         let distance=station[STATION_DISTANCE] as? NSString
+        let longitude=station[LONGITUDE] as? NSString
+        let latitude=station[LATITUDE] as? NSString
+        let stationLaneCodes=station[STATION_LANE_CODES] as? NSString
+        var stationLineCodes=station[STATION_LINE_CODES] as? NSString
+        let travelTime=station[TRAVELTIME] as? NSString
+        let color=station[COLOR] as? NSString
+        stationLineCodes = stationLineCodes!.trimmingCharacters(in: .whitespacesAndNewlines) as NSString
         
        // print(name!)
         //print(code!)
@@ -341,6 +352,14 @@ func createStationData()
         sqlite3_bind_text(insertStatement, 1, name!.utf8String, -1, SQLITE_TRANSIENT);
         sqlite3_bind_text(insertStatement, 2, code!.utf8String , -1, SQLITE_TRANSIENT);
         sqlite3_bind_text(insertStatement, 3, distance!.utf8String , -1, SQLITE_TRANSIENT);
+        
+        sqlite3_bind_text(insertStatement, 4, longitude!.utf8String, -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(insertStatement, 5, latitude!.utf8String , -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(insertStatement, 6, stationLaneCodes!.utf8String , -1, SQLITE_TRANSIENT);
+        
+        sqlite3_bind_text(insertStatement, 7, stationLineCodes!.utf8String, -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(insertStatement, 8, travelTime!.utf8String , -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(insertStatement, 9, color!.utf8String , -1, SQLITE_TRANSIENT);
         
         let returnCode=sqlite3_step(insertStatement)
         if (returnCode != SQLITE_DONE)
@@ -362,7 +381,7 @@ func createStationData()
 func getStationData(fromStation:String,toStation:String) -> StationData
 {
     
-    prepareMrtStationStatement()
+    
 
     let stationData:StationData = StationData()
     
@@ -370,53 +389,244 @@ func getStationData(fromStation:String,toStation:String) -> StationData
     stationData.toStation=toStation
     let fromStr = fromStation as NSString?
     let toStr = toStation as NSString?
-    sqlite3_bind_text(selectStatement, 1, fromStr!.utf8String, -1, SQLITE_TRANSIENT);
     
-    sqlite3_bind_text(selectStatement, 2, toStr!.utf8String, -1, SQLITE_TRANSIENT);
-
+    var stations:[NSString] = [NSString] ()
+    stations.append(fromStr!)
+    stations.append(toStr!)
+    
+    var stationModels:[StationModel] = [StationModel] ()
+    print(stations.count)
     var distances = [String]()
     var codes = [String]()
     var names = [String]()
     var idArr = [Int]()
     
-    
-    while (sqlite3_step(selectStatement) == SQLITE_ROW)
-    {
-    
-        let code_buf = sqlite3_column_text(selectStatement, 0)
-        let code = String(cString: code_buf!)
-        let dist_buf = sqlite3_column_text(selectStatement, 1)
-        let distance = String(cString: dist_buf!)
-        let name_buf = sqlite3_column_text(selectStatement, 2)
-        let name = String(cString: name_buf!)
-        let id_buf = sqlite3_column_text(selectStatement, 3)
-        let id:Int! = Int(String(cString: id_buf!))
-        idArr.append(id)
-
-        codes.append(code)
-        names.append(name)
-    
-        distances.append(distance)
+    for station in stations{
+        
+        
+        print("enter \(station)")
+        prepareMrtStationStatement()
+        sqlite3_bind_text(selectStatement, 1, station.utf8String, -1, SQLITE_TRANSIENT);
+        
+        if (sqlite3_step(selectStatement) == SQLITE_ROW)
+        {
+            
+            print("enter inside")
+            let stationModel:StationModel = StationModel()
+            
+            // stationModel.stationName
+            
+            // stationModel.stationCode
+            
+            let code_buf = sqlite3_column_text(selectStatement, 0)
+            stationModel.stationCode = String(cString: code_buf!)
+            let dist_buf = sqlite3_column_text(selectStatement, 1)
+            let distanceStr = String(cString: dist_buf!)
+            stationModel.stationDistance = distanceStr.components(separatedBy: ",")
+            
+            let name_buf = sqlite3_column_text(selectStatement, 2)
+            stationModel.stationName = String(cString: name_buf!)
+            let id_buf = sqlite3_column_text(selectStatement, 8)
+            
+            stationModel.stationPrimaryId = Int(String(cString: id_buf!))
+            print(stationModel.stationPrimaryId)
+            
+            
+            let color_buf = sqlite3_column_text(selectStatement, 3)
+            let colors = String(cString: color_buf!)
+            
+            print(colors)
+            
+            stationModel.colors = colors.components(separatedBy: ",")
+            
+            print("station colors \(stationModel.colors)")
+            
+            let station_line_ids = sqlite3_column_text(selectStatement, 4)
+            let stationLinesStr = String(cString: station_line_ids!)
+            print("stationLinecodesstr \(stationLinesStr)")
+            stationModel.stationLineCodes = stationLinesStr.components(separatedBy: " ")
+            
+            print("stationLinecodesarr \(stationModel.stationLineCodes)")
+            
+            
+            
+            let stationCodes = sqlite3_column_text(selectStatement, 5)
+            let stationCodesStr = String(cString: stationCodes!)
+            stationModel.stationLaneCodes = stationCodesStr.components(separatedBy: ",")
+            print("stationLinecodesstr \(stationModel.stationLaneCodes)")
+            
+            stationModels.append(stationModel)
+            print("stationLinecodesstr \(stationLinesStr)")
+            sqlite3_reset(selectStatement);
+            sqlite3_clear_bindings(selectStatement);
+            //codes.append(code)
+           // names.append(name)
+            
+            //distances.append(distance)
+        }
     }
-    stationData.totalDistance = abs(Double(distances[0])! - Double(distances[1])!)
     
-    stationData.fare = getFare(distance: stationData.totalDistance)
+    print(stationModels.count)
+    let fromStationModel:StationModel = stationModels[0]
+    let toStationModel:StationModel = stationModels[1]
     
-    if(idArr[names.index(of: fromStation)!] > idArr[names.index(of: toStation)!])
+    var isSameLine:Bool = false
+    print("reached")
+    print("fromstation color \(fromStationModel.colors)")
+    print("tostation color \(toStationModel.colors)")
+    var lane:String = String()
+    for templane in fromStationModel.stationLaneCodes
+    {
+        if(toStationModel.stationLaneCodes.contains(templane))
+        {
+            lane = templane
+            isSameLine = true
+            
+            print("Same Lane data")
+            
+            
+            distances.append(fromStationModel.stationDistance[fromStationModel.stationLaneCodes.index(of: lane)!])
+            distances.append(toStationModel.stationDistance[toStationModel.stationLaneCodes.index(of: lane)!])
+            
+            stationData.totalDistance = abs(Double(distances[0])! - Double(distances[1])!)
+            
+            stationData.fare = getFare(distance: stationData.totalDistance)
+            
+            print("reached next step")
+            
+            
+            
+            
+            if(fromStationModel.stationPrimaryId > toStationModel.stationPrimaryId)
+            {
+                stationData.stationDirectionId=1
+            }
+            else
+            {
+                stationData.stationDirectionId=2
+            }
+            
+            stationData.stationCode = fromStationModel.stationCode
+
+            break;
+        }
+        else
+        {
+            
+       // fromStationModel.stationLineCodes[fromStationModel.stationLaneCodes.index(of: lane)]
+      //let connectingStations:[String] = getConnectingLane(lane: fromStationModel.stationLaneCodes.remove(at: fromStationModel.stationLaneCodes.index(of: lane)))
+            
+           // var connectingLanes:String = [String]()
+            var laneArray:[String] = [String] ()
+            for stationModel in stationModels
+            {
+                laneArray.insert(contentsOf: stationModel.stationLaneCodes, at: laneArray.count)
+            }
+            laneArray = removeDuplicates(array: laneArray)
+            
+            getConnectingLane(lanes: laneArray)
+            
+        }
+    }
+    
+       /*if(idArr[names.index(of: fromStation)!] > idArr[names.index(of: toStation)!])
     {
      stationData.stationDirectionId=1
     }
     else
     {
         stationData.stationDirectionId=2
+    }*/
+    
+    if(stationData.stationDirectionId == nil)
+    {
+        stationData.stationDirectionId = 1
     }
     
     
-    stationData.stationCode=codes[names.index(of: fromStation)!]
+   // stationData.stationCode=codes[names.index(of: fromStation)!]
   
     sqlite3_reset(selectStatement);
     sqlite3_clear_bindings(selectStatement);
     
     return stationData
+}
+
+
+func getStationDirectionsData(fromStationModel:StationModel, toStationModel:StationModel)
+{
+    
+}
+
+func getConnectingLane(lanes: [String]) -> [StationModel]
+{
+    
+    prepareCheckpointStatement()
+   
+    
+    print("intermediate lanes \(lanes)")
+    
+    //var queryString  =
+    
+    var intermediateStations:[StationModel] = [StationModel] ()
+
+    
+   /* for lane in lanes
+    {
+    
+     let laneStr = lane as NSString?
+    
+    sqlite3_bind_text(selectStatement, 1, laneStr!.utf8String, -1, SQLITE_TRANSIENT);
+    print("lane code \(lane)")
+    while(sqlite3_step(selectStatement) == SQLITE_ROW)
+    {
+        
+        let intermediateStation:StationModel = StationModel()
+        
+        let name_buf = sqlite3_column_text(selectStatement, 0)
+        intermediateStation.stationName =  String(cString: name_buf!)
+
+        
+        let lineid_buf = sqlite3_column_text(selectStatement, 1)
+        let lineid = String(cString: lineid_buf!)
+        intermediateStation.stationLineCodes = [lineid]
+       // print("checkpoint stations id \(id)")
+        
+        let lane_buf = sqlite3_column_text(selectStatement, 2)
+        let lane_code = String(cString: lane_buf!)
+        //intermediateStation.stationLaneCodes = [lane_code]
+        //intermediateStations.append(intermediateStation)
+        
+        
+        
+        print("checkpoint stations lanecode \(lane_code)")
+    }
+    }
+   /* else
+    {
+        print(ERROR_CODE, sqlite3_errcode (mrtDb));
+        let error = String(cString: sqlite3_errmsg(mrtDb));
+        print(ERROR_MESSAGE, error);
+    }*/
+    sqlite3_reset(selectStatement);
+    sqlite3_clear_bindings(selectStatement);*/
+
+    
+    return intermediateStations
+    
+}
+
+func removeDuplicates(array:[String]) -> [String]
+{
+    var items:[String] = [String] ()
+    for item in array
+    {
+        if(!items.contains(item))
+        {
+            items.append(item)
+        }
+    }
+    
+    return items
 }
 
